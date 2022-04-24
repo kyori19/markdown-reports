7a5723db61d874e190fbc08ef773b10482bfe1b9
kyori19 <kyori@accelf.net>
2022-04-24 05:50:37 +0000

    Extract extendImageRule

--- src/lib/image-caption/index.ts
+++ src/lib/image-caption/index.ts
@@ -1,18 +1,23 @@
-import { PluginSimple } from "markdown-it";
+import MarkdownIt, { PluginSimple } from "markdown-it";
+import type Renderer from "markdown-it/lib/renderer";
 
 const imageCaptionPlugin: PluginSimple = (md) => {
-  const defaultImageRule = md.renderer.rules.image || ((tokens, idx, options, _, self) => self.renderToken(tokens, idx, options));
-  md.renderer.rules.image = (tokens, idx, options, env, self) => {
+  extendImageRule(md, (defaultRule) => (tokens, idx, options, env, self) => {
     const token = tokens[idx];
     const alt = token.content;
 
     return `
       <figure>
-        ${defaultImageRule(tokens, idx, options, env, self)}
+        ${defaultRule(tokens, idx, options, env, self)}
         <figcaption align="center">${alt}</figcaption>
       </figure>
     `;
-  };
+  });
+};
+
+const extendImageRule = (md: MarkdownIt, customRule: (defaultRule: Renderer.RenderRule) => Renderer.RenderRule) => {
+  const defaultRule = md.renderer.rules.image || ((tokens, idx, options, _, self) => self.renderToken(tokens, idx, options));
+  md.renderer.rules.image = customRule(defaultRule);
 };
 
 export default imageCaptionPlugin;
